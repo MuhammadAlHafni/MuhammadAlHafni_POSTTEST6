dbdd1ede11f95a9ce37c3532568314d2cb219119
MuhammadAlHafni <145321599+MuhammadAlHafni@users.noreply.github.com>
2023-10-25 00:26:42 +0700

    Delete POSTTEST6 directory

--- POSTTEST6/php.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleModeButton = document.querySelector("#toggle-mode");
-    const body = document.body;
-    
-    toggleModeButton && toggleModeButton.addEventListener("click", function() {
-        body.classList.toggle("light-mode");
-        body.classList.toggle("dark-mode");
-    });
-});
